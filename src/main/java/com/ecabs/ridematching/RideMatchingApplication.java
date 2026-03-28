0b584a37ab6e4cb491a442e325863ccb4e01a8bd
Kayleigh Agius <174701071+kayleighcodingthings@users.noreply.github.com>
2026-03-28 11:23:00 +0100

    Add SpringApplication

---
+++ src/main/java/com/ecabs/ridematching/RideMatchingApplication.java
@@ -0,0 +1,12 @@
+package com.ecabs.ridematching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideMatchingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RideMatchingApplication.class, args);
+    }
+}
